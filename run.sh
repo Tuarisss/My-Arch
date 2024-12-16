84958d8e21add43f1f5e45e75dd33c4391211279
Tuaris <77112839+Tuarisss@users.noreply.github.com>
2024-12-16 20:16:20 +0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,47 +1,55 @@
 #!/bin/bash
 
-# Проверка, запущен ли скрипт от имени root
 if [ "$EUID" -ne 0 ]; then
-    echo "Пожалуйста, запустите скрипт от имени root (с помощью sudo)."
+    echo "Please run the script as root (using sudo)."
     exit 1
 fi
 
-# Функция для проверки, установлен ли пакет
 check_package_installed() {
     pacman -Q $1 &>/dev/null
 }
 
-# Установка необходимых пакетов для сборки yay
 install_yay_dependencies() {
-    echo "Устанавливаем зависимости для yay..."
     pacman -S --needed --noconfirm git base-devel
 }
 
-# Установка yay, если он не установлен
 if ! command -v yay &>/dev/null; then
-    echo "yay не найден, начинаем установку..."
     install_yay_dependencies
-
-    # Клонируем репозиторий yay
     git clone https://aur.archlinux.org/yay.git /tmp/yay
-
-    # Сборка и установка yay
     cd /tmp/yay || exit
     makepkg -si --noconfirm
-    
-    # Удаляем временные файлы
     cd ~ || exit
     rm -rf /tmp/yay
-else
-    echo "yay уже установлен."
 fi
 
-# Установка пакетов через yay
-PACKAGES=(wireshark-cli wireshark-qt wifite hashcat kde-applications)
+PACKAGES=(
+    wireshark-cli
+    wireshark-qt
+    wifite
+    hashcat
+    kde-applications
+    flatpak
+    google-chrome
+    nmap
+    metasploit
+    john
+    aircrack-ng
+    burpsuite
+    hydra
+    sqlmap
+    nikto
+    zaproxy
+    openvpn
+    gobuster
+    binwalk
+    radare2
+    tcpdump
+    dirb
+    dirsearch
+)
 
-echo "Устанавливаем пакеты: ${PACKAGES[*]}"
 for package in "${PACKAGES[@]}"; do
     yay -S --needed --noconfirm "$package"
 done
 
-echo "Все пакеты успешно установлены!"
+echo "All packages installed successfully!"
